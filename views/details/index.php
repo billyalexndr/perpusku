2fd0e97f07419b7a5cfa435a46bbaa9feedf90bf
billyalx <billyalexndr@gmail.com>
2023-05-23 08:36:47 +0700

    update logic baca buku

--- views/details/index.php
+++ views/details/index.php
@@ -15,7 +15,7 @@
                 $id_buku = mysqli_real_escape_string($conn, $_GET['id_buku']);
 
                 // Query untuk mengambil data buku berdasarkan ID buku
-                $query = "SELECT * FROM buku WHERE id_buku = '$id_buku'";
+                $query = "SELECT buku.*, kategori.icon_kategori FROM buku JOIN kategori ON buku.id_kategori = kategori.id_kategori WHERE buku.id_buku = '$id_buku'";
                 $result = mysqli_query($conn, $query);
 
                 // ...
@@ -24,18 +24,27 @@
                     $row = mysqli_fetch_assoc($result);
                     ?>
                     <!-- Tampilkan informasi buku -->
-                    <!-- <img src="<?= $row['cover_buku']; ?>" alt="" class="float-left thumbnail" width="250"> -->
                     <img src="../../assets/images/cover-buku/<?=$row['cover_buku']; ?>" alt="" class="float-left thumbnail" width="250">
                     <div class="banner-detail">
-                    <h1 class="judul"><?= $row['judul_buku']; ?></h1>
-                    <hr class="my-4">
-                    <p class="kategori"><img src="../../assets/images/cover-buku/<?=$row['logo_kategori']; ?>" alt="" width="26px"> <?= $row['genre_buku']; ?></p>
-                    <p class="penulis">by <?= $row['penulis_buku']; ?></p>
-                    <p class="penerbit">Publisher: <?= $row['penerbit_buku']; ?></p>
-                    <p class="penerbit">Publication Year: <?= $row['tanggal_terbit']; ?></p>
-                    <a class="btn btn-danger btn-baca-login" href="#" role="button">HARAP LOGIN</a>
-                    <a class="btn btn-success btn-baca" href="./baca.php?id_buku=<?= $row['id_buku']; ?>" role="button">MULAI BACA</a>
-                    <a class="btn btn-danger btn-baca-login ml-5" href="../home/" role="button">Back to Home</a>
+                        <h1 class="judul"><?= $row['judul_buku']; ?></h1>
+                        <hr class="my-4">
+                        <p class="kategori"><img src="../../assets/images/icon-kategori/<?=$row['icon_kategori']; ?>" alt="" width="26px"> <?= $row['genre_buku']; ?></p>
+                        <p class="penulis">by <?= $row['penulis_buku']; ?></p>
+                        <p class="penerbit">Publisher: <?= $row['penerbit_buku']; ?></p>
+                        <p class="penerbit">Publication Year: <?= $row['tanggal_terbit']; ?></p>
+                        <?php
+                        // Cek apakah user sudah login
+                        if (isset($_SESSION['username'])) {
+                            ?>
+                            <a class="btn btn-success btn-baca" href="./baca.php?id_buku=<?= $row['id_buku']; ?>" role="button">MULAI BACA</a>
+                            <?php
+                        } else {
+                            ?>
+                            <a class="btn btn-danger btn-baca-login" href="../login/login.php" role="button">HARAP LOGIN</a>
+                            <?php
+                        }
+                        ?>
+                        <a class="btn btn-danger btn-baca-login ml-5" href="../home/" role="button">Back to Home</a>
                     </div>
 
                     <?php
@@ -48,6 +57,7 @@
 
             // ...
             ?>
+
         </div>
     </div>
 
